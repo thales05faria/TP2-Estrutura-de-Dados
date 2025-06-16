d67c443df17c1a79fbe797c0433e23b116d678e0
thales05faria <thales05faria@gmail.com>
2025-06-16 17:13:57 -0300

    Implementação Pilha

---
+++ include/Pilha.hpp
@@ -0,0 +1,30 @@
+#ifndef PILHA_HPP
+#define PILHA_HPP
+#include "Pacote.hpp"
+
+class Pilha{
+
+    private: 
+
+        struct CelulaPilha{
+            Pacote item; //Armazena um pacote
+            CelulaPilha *next;
+        };
+
+        CelulaPilha *topo;
+        int tamanho;
+
+    public:
+
+        Pilha();
+        ~Pilha();
+
+        void Empilha(const Pacote& item);
+        void Desempilha();
+        Pacote* getTopo() const;
+        int getTamanho() const;
+        void Limpa();
+
+};
+
+#endif PILHA_HPP
---
+++ src/Pilha.cpp
@@ -0,0 +1,44 @@
+#include "Pilha.hpp"
+#include <iostream>
+
+Pilha::Pilha(){
+    topo = nullptr;
+    tamanho = 0;
+}
+
+Pilha::~Pilha(){
+    Limpa();
+}
+
+int Pilha::getTamanho() const{
+    return tamanho;
+}
+
+Pacote* Pilha::getTopo() const{
+    if (topo == nullptr) return nullptr;
+    return &(topo->item);
+}
+
+void Pilha::Limpa(){
+    while(topo != nullptr) Desempilha();
+}
+
+void Pilha::Empilha(const Pacote& item){
+
+    CelulaPilha *nova = new CelulaPilha();
+    nova->item = item;
+    nova->next = topo;
+    topo = nova;
+    tamanho++;
+
+}
+
+void Pilha::Desempilha(){
+    if (topo == nullptr) return;
+
+    CelulaPilha *antigoTopo = topo;
+    topo = topo->next;
+    delete antigoTopo;
+    tamanho--;
+
+}
