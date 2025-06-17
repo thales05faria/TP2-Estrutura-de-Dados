673305fb57fdbcbcfab0ab7f48fbaecd0fb3744e
thales05faria <thales05faria@gmail.com>
2025-06-17 11:09:45 -0300

    Implementação Pacote

---
+++ include/Pacote.hpp
@@ -0,0 +1,52 @@
+#ifndef PACOTE_HPP
+#define PACOTE_HPP
+#include "ListaEncadeada.hpp"
+
+
+enum EstadoPacote {
+    NAO_POSTADO,
+    CHEGADA_ESCALONADA,
+    ARMAZENADO,
+    REMOVIDO_PARA_TRANSPORTE,
+    ENTREGUE
+};
+
+class Pacote{
+
+    public: 
+        Pacote();
+        Pacote(int id, int origem, int destino, int tempoPostagem);
+        ~Pacote();
+
+        int getID() const;
+        int getOrigem() const;
+        int getDestino() const;
+        EstadoPacote getEstado() const;
+
+        void setRota(const ListaEncadeada& novaRota);
+        void setEstado(EstadoPacote novoEstado, int tempoAtual);
+        void setLocalizacao(int novaLocalizacao);
+
+        int* getProximoArmazem();
+        void deslocamentoRota();
+
+        int getTempoArmazenado() const;
+        int getTempoEmTransporte() const;
+
+    private:
+        int id;
+        int origem;
+        int destino;
+        int tempoPostagem;
+
+        EstadoPacote estadoPacote;
+        ListaEncadeada rota;
+        int localizacao;
+
+        int tempoArmazenado;
+        int tempoEmTransporte;
+        int ultimoTempo;
+
+};
+
+#endif PACOTE_HPP
---
+++ src/Pacote.cpp
@@ -0,0 +1,82 @@
+#include "Pacote.hpp"
+        
+Pacote::Pacote(){
+    this->id = -1;
+    this->origem = -1;
+    this->destino = -1;
+    this->tempoPostagem = -1;
+    this->estadoPacote = NAO_POSTADO;
+    this->localizacao = -1;
+    this->tempoArmazenado = 0;
+    this->tempoEmTransporte = 0;
+    this->ultimoTempo = 0;
+}
+
+Pacote::Pacote(int id, int origem, int destino, int tempoPostagem){
+    this->id = id;
+    this->origem = origem;
+    this->destino = destino;
+    this->tempoPostagem = tempoPostagem;
+
+    this->estadoPacote = NAO_POSTADO;
+    this->localizacao = origem;
+    this->tempoArmazenado = 0;
+    this->tempoEmTransporte = 0;
+    this->ultimoTempo = tempoPostagem;
+}
+
+Pacote::~Pacote(){
+
+}
+
+int Pacote::getID() const{
+    return id;
+}
+
+int Pacote::getOrigem() const{
+    return origem;
+}
+int Pacote::getDestino() const{
+    return destino;
+}
+EstadoPacote Pacote::getEstado() const{
+    return estadoPacote;
+}
+
+void Pacote::setRota(const ListaEncadeada& novaRota){
+    rota = novaRota;
+}
+
+void Pacote::setEstado(EstadoPacote novoEstado, int tempoAtual){
+    int duracaoEstado = tempoAtual - ultimoTempo;
+
+    if(estadoPacote == ARMAZENADO){
+        tempoArmazenado += duracaoEstado;
+    }
+
+    if(estadoPacote == REMOVIDO_PARA_TRANSPORTE){
+        tempoEmTransporte += duracaoEstado;
+    }
+
+    estadoPacote = novoEstado;
+    ultimoTempo = tempoAtual;
+}
+
+void Pacote::setLocalizacao(int novaLocalizacao){
+    localizacao = novaLocalizacao;
+}
+
+int* Pacote::getProximoArmazem(){
+    return rota.GetFirst();
+}
+
+void Pacote::deslocamentoRota(){
+    rota.RemoveInicio();
+}
+
+int Pacote::getTempoArmazenado() const{
+    return tempoArmazenado;
+}
+int Pacote::getTempoEmTransporte() const{
+    return tempoEmTransporte;
+}
