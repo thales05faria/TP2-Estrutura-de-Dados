e48f4011861c0b36189d2dc96c45c85507ffcfab
thales05faria <thales05faria@gmail.com>
2025-06-24 20:07:19 -0300

    Adição de comentários ao código

--- src/Pilha.cpp
+++ src/Pilha.cpp
@@ -1,28 +1,35 @@
 #include "Pilha.hpp"
 #include <iostream>
 
+//Construtor
 Pilha::Pilha(){
     topo = nullptr;
     tamanho = 0;
 }
 
+
+//Destrutor
 Pilha::~Pilha(){
     Limpa();
 }
 
+//Retorna o tamanho
 int Pilha::getTamanho() const{
     return tamanho;
 }
 
+//Retorna o item do topo a pilha
 Pacote* Pilha::getTopo() const{
     if (topo == nullptr) return nullptr;
     return topo->item;
 }
 
+//Remove todos os itens da pilha
 void Pilha::Limpa(){
     while(topo != nullptr) Desempilha();
 }
 
+//Adiciona um item ao topo da pilha
 void Pilha::Empilha(Pacote* item){
 
     CelulaPilha *nova = new CelulaPilha();
@@ -33,6 +40,7 @@ void Pilha::Empilha(Pacote* item){
 
 }
 
+//Remove um item do topo da pilha
 void Pilha::Desempilha(){
     if (topo == nullptr) return;
 
